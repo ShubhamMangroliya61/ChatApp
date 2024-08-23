37ae69cc03a320df7602f8d2dd06ed2b89982f6e
shubhamtatvasoft <shubham.mangroliya@etatvasoft.com>
2024-08-23 11:39:49 +0530

    fix error

--- ChatAppFrontEnd/src/App.jsx
+++ ChatAppFrontEnd/src/App.jsx
@@ -1,18 +1,30 @@
-import './App.css'
-import { useDispatch } from 'react-redux';
+import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 import { resetError, useSelectorUserState } from './redux/slice/AuthSlice';
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { AuthRoutes, NormalRoutes } from './routes/CustomRoutes';
+import { isTokenValid } from './utils/AuthService';
 
 const App = () => {
   const dispatch = useDispatch();
   const location = useLocation();
   const { isLoggedIn } = useSelectorUserState();
+  
+  const validateToken = () => {
+    const token = localStorage.getItem("token");
+    return token ? isTokenValid() : false;
+  };
+
+  const RoutesComponent = useMemo(() => (
+    validateToken() ? <NormalRoutes /> : <AuthRoutes />
+  ), [validateToken()]);
+
   useEffect(() => {
     dispatch(resetError());
-  },[location.pathname])
-  return <> {isLoggedIn ? <NormalRoutes /> : <AuthRoutes />}</>
+  }, [dispatch, location.pathname]);
 
-}
-export default App
+  return <>{RoutesComponent}</>;
+};
+
+export default App;
