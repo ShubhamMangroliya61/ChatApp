455d151a1ab6cc1b09fa96398eaeb9fffc510451
shubhamtatvasoft <shubham.mangroliya@etatvasoft.com>
2024-08-23 11:59:08 +0530

    Add changes

--- ChatAppFrontEnd/src/App.jsx
+++ ChatAppFrontEnd/src/App.jsx
@@ -1,10 +1,10 @@
-import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { useLocation } from 'react-router-dom';
-import { resetError, useSelectorUserState } from './redux/slice/AuthSlice';
-import { useEffect, useMemo } from 'react';
-import { AuthRoutes, NormalRoutes } from './routes/CustomRoutes';
-import { isTokenValid } from './utils/AuthService';
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
+import { resetError, useSelectorUserState } from "./redux/slice/AuthSlice";
+import { useEffect, useMemo } from "react";
+import { AuthRoutes, NormalRoutes } from "./routes/CustomRoutes";
+import { jwtDecode } from "jwt-decode";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -14,9 +14,29 @@ const App = () => {
     return token ? isTokenValid() : false;
   };
 
-  const RoutesComponent = useMemo(() => (
-    validateToken() ? <NormalRoutes /> : <AuthRoutes />
-  ), [validateToken()]);
+  const decodeToken = () => {
+    const token = localStorage.getItem("token");
+    let finalDecodedToken = null;
+    try {
+      finalDecodedToken = token ? jwtDecode(token) : null;
+    } catch (error) {
+      finalDecodedToken = null;
+    }
+    return finalDecodedToken;
+  };
+
+  const isTokenValid = () => {
+    const expiryTime = decodeToken()?.exp;
+    if (expiryTime) {
+      return 1000 * expiryTime > new Date().getTime();
+    } else {
+      return false;
+    }
+  };
+  const RoutesComponent = useMemo(
+    () => (validateToken() ? <NormalRoutes /> : <AuthRoutes />),
+    [validateToken()]
+  );
 
   useEffect(() => {
     dispatch(resetError());
